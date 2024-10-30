61bf07283ed0570f23f3d2409e335e35fa71b296
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-10-30 15:56:36 +0800

    feat(web): update project

--- web/src/app/projects/page.tsx
+++ web/src/app/projects/page.tsx
@@ -39,10 +39,10 @@ export default function Projects() {
             <div className="lg:pr-8 lg:pt-4">
               <div className="lg:max-w-lg">
                 <h2 className="text-base font-semibold leading-7 text-indigo-600">
-                  The Best Video Downloader
+                  The Best NO-CODE AI/ML/DL Training Platform
                 </h2>
                 <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                  LinkDown
+                  SIMPLE-AI
                 </p>
                 <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                   {features.map((feature) => (

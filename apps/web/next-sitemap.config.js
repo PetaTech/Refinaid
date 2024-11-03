3fc6f25ff5b235200c6573bd280f6d6f40cef193
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-03 17:14:11 +0800

    feat(web): update sitemap config

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://linkscape.app",
+  siteUrl: process.env.SITE_URL || "https://refinaid.vercel.app",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };

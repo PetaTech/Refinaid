570fbafbe9d9a29b826c69f2f9e6699728d69299
Hugo ChunHo Lin <hugo970217@gmail.com>
2024-11-15 21:26:45 +0800

    feat(web): enhance project page with chatger section

--- apps/web/src/app/projects/page.tsx
+++ apps/web/src/app/projects/page.tsx
@@ -82,6 +82,52 @@ export default function Projects() {
               height={626}
               alt=""
             />
+            <div className="lg:pr-8 lg:pt-4">
+              <div className="lg:max-w-lg">
+                <h2 className="text-base font-semibold leading-7 text-indigo-600 mt-8 sm:mt-0">
+                  Master Coding with Your AI Tutor
+                </h2>
+                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                  Chatger
+                </p>
+                <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                  {features.map((feature) => (
+                    <div key={feature.name} className="relative pl-9">
+                      <dt className="inline font-semibold text-gray-900">
+                        <feature.icon
+                          className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                          aria-hidden="true"
+                        />
+                        {feature.name}
+                        <br />
+                      </dt>{" "}
+                      <dd className="inline">{feature.description}</dd>
+                    </div>
+                  ))}
+                  <div className="flex flex-row">
+                    <Link href="https://github.com/1chooo/refinaid/releases">
+                      <button className="mr-3 mt-2 flex items-center rounded-md bg-black px-3 py-2 text-white">
+                        <FaDownload className="mr-1.5 h-5 w-5" />
+                        <div className="font-semibold">Download</div>
+                      </button>
+                    </Link>
+                    <Link href="https://github.com/1chooo/refinaid">
+                      <button className="mr-3 mt-2 flex items-center rounded-md bg-black px-3 py-2 text-white">
+                        <FaGithub className="mr-1.5 h-5 w-5" />
+                        <div className="font-semibold">GitHub</div>
+                      </button>
+                    </Link>
+                  </div>
+                </dl>
+              </div>
+            </div>
+            <Image
+              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 md:block lg:-ml-0"
+              src="/chager.png"
+              width={1113}
+              height={626}
+              alt=""
+            />
           </div>
         </div>
       </div>

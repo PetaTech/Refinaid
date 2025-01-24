8c3f14229271b672e8b0112c0871980cea34673d
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-01-24 18:47:53 +0800

    chore(web): remove deprecated files and scripts

--- apps/chatger/test/test_gemini.sh
+++
@@ -1,4 +0,0 @@
-curl \
-  -H 'Content-Type: application/json' \
-  -d '{"contents":[{"parts":[{"text":"Write a story about a magic backpack"}]}]}' \
-  -X POST https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=YOUR_GOOGLE_GEMINI_API_KEY
